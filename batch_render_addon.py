d85a6e92b4464d0837eabe214378527a97b05999
liyouwang <1085066875@qq.com>
2020-11-25 16:31:24 +0800

    add batch convert fbx to abc

--- batch_render_addon.py
+++ batch_render_addon.py
@@ -1,16 +1,14 @@
 bl_info = {
-    "name": "Batch Render",
+    "name": "Batch Export",
     "author": "wangliyou",
-    "version": (1, 1),
+    "version": (1, 2),
     "blender": (2, 83, 0),
-    "location": "View3D > Toolbar > Batch Render",
-    "description": "batch render video",
+    "location": "View3D > Toolbar > Batch Export",
+    "description": "batch export abc or video",
     "warning": "",
     "wiki_url": "",
     "category": "Add Mesh",
 }
- 
- 
  
 import bpy
 import os
@@ -18,7 +16,7 @@ import os
  
     #This is the Main Panel (Parent of Panel A and B)
 class MainPanel(bpy.types.Panel):
-    bl_label = "Batch Render"
+    bl_label = "Batch Export Tool"
     bl_idname = "VIEW_PT_MainPanel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -27,14 +25,66 @@ class MainPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         row = layout.row()
-        row.label(text= "Batch Render", icon= 'OBJECT_ORIGIN')
+        row.label(text= "Batch ExPORT TOOL", icon= 'OBJECT_ORIGIN')
         row = layout.row()
-        row.operator("wm.batch_render", icon= 'CUBE', text= "batch_render")
- 
+        row.operator("wm.batch_render", icon= 'CUBE', text= "batch render")
+        row = layout.row()
+        row.operator("wm.batch_export_abc", icon= 'CUBE', text= "batch export abc")
+        
+        
+class WM_OT_batch_export_abc(bpy.types.Operator):
+    
+    bl_label = "batch export abc box"
+    bl_idname = "wm.batch_export_abc"
+    
+    input_dir = bpy.props.StringProperty(name= "input dir", default= "")
+    output_dir = bpy.props.StringProperty(name= "output dir:", default= "")
+    
+    def execute(self, context):
+        
+        input_dir = self.input_dir
+        print("input_dir is {}".format(input_dir))
+        output_dir = self.output_dir
+        print("output_dir is {}".format(output_dir))
+        names = os.listdir(input_dir)
+        for name in names:
+            input_path = os.path.join(input_dir, name)
+            self.import_fbx(input_path)
+            output_path = os.path.join(output_dir, name[:-3]+'abc')
+            self.export(output_path)
+            print("-" * 100)
+            print("{} export finish".format(name))
+            
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+       
+        return context.window_manager.invoke_props_dialog(self)
+    
+    def set_frame_number(self):
+        start_number = bpy.data.objects['head_geo'].animation_data.action.frame_range[0]
+        end_number = bpy.data.objects['head_geo'].animation_data.action.frame_range[1]
+        bpy.context.scene.frame_start = start_number
+        bpy.context.scene.frame_end = end_number
+        
+    def import_fbx(self, file):
+        bpy.ops.import_scene.fbx(filepath=file, global_scale=0.01) # bas
+        bpy.context.selected_objects[0].name ='head_geo'
+        
+    def export(self, output_path):
+        
+        for i in range(1, len(bpy.data.objects['head_geo'].data.shape_keys.key_blocks)):
+            bpy.data.objects['head_geo'].data.shape_keys.key_blocks[i].slider_min = -5
+            bpy.data.objects['head_geo'].data.shape_keys.key_blocks[i].slider_max = 5
+        self.set_frame_number()
+        bpy.data.objects['head_geo'].select_set(True)
+        bpy.ops.wm.alembic_export(filepath=output_path, selected=True)
+        bpy.ops.object.delete(use_global=False)
+        
  
 class WM_OT_batch_render(bpy.types.Operator):
-    """Open the Add Cube Dialog box"""
-    bl_label = "Add Cube Dialog Box"
+    
+    bl_label = "batch export render box"
     bl_idname = "wm.batch_render"
    
     input_dir = bpy.props.StringProperty(name= "input dir", default= "")
@@ -56,7 +106,6 @@ class WM_OT_batch_render(bpy.types.Operator):
             self.render_animation(output_path)
             print("-" * 100)
             print("{} render finish".format(name))
-       
        
         return {'FINISHED'}
    
@@ -114,21 +163,19 @@ class WM_OT_batch_render(bpy.types.Operator):
     def import_fbx(self, file):
         bpy.ops.import_scene.fbx(filepath=file, global_scale=0.01) # bas
         bpy.context.selected_objects[0].name ='head_geo'
-     
-     
-     
+       
            
         #Here we are Registering the Classes        
 def register():
     bpy.utils.register_class(MainPanel)
     bpy.utils.register_class(WM_OT_batch_render)
-   
-   
- 
+    bpy.utils.register_class(WM_OT_batch_export_abc)
+    
     #Here we are UnRegistering the Classes    
 def unregister():
     bpy.utils.unregister_class(MainPanel)
     bpy.utils.unregister_class(WM_OT_batch_render)
+    bpy.utils.unregister_class(WM_OT_batch_export_abc)
        
     #This is required in order for the script to run in the text editor    
 if __name__ == "__main__":
